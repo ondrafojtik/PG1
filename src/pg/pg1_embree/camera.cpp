53cff08fd03cfda1fa3c21e70c43ed4f258233ed
Ondrej Fojtik <fojtik.ondrej@seznam.cz>
2021-09-18 12:44:03 +0200

    somewhat working ex1

---
+++ src/pg/pg1_embree/camera.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "camera.h"
+
+#include "vector3.h"
+// stredove promitani 
+// Z jde zpet (do oka)
+// Y je nahoru
+
+// CS - camera system
+// WS - world system
+
+Camera::Camera( const int width, const int height, const float fov_y,
+	const Vector3 view_from, const Vector3 view_at )
+{
+	width_ = width;
+	height_ = height;
+	fov_y_ = fov_y;
+
+	view_from_ = view_from;
+	view_at_ = view_at;
+
+
+	Vector3 _forward = view_at - view_from;
+	_forward.Normalize();
+	Vector3 x_cs = up_.CrossProduct(_forward);
+	Vector3 y_cs = _forward.CrossProduct(x_cs);
+
+	// TODO compute focal lenght based on the vertical field of view and the camera resolution
+	f_y_ = height_ / (2 * tan(fov_y_ / 2));
+	// TODO build M_c_w_ matrix	
+	M_c_w_ = Matrix3x3(x_cs, y_cs, _forward);
+}
+
+RTCRay Camera::GenerateRay( const float x_i, const float y_i ) const
+{
+	RTCRay ray = RTCRay();
+
+	// TODO fill in ray structure and compute ray direction
+	// ray.org_x = ...
+
+	// ray setup
+	ray.org_x = view_from_.x;
+	ray.org_y = view_from_.y;
+	ray.org_z = view_from_.z;
+	ray.tnear = FLT_MIN; // start of ray segment
+	Vector3 target{x_i - (width_ / 2), y_i - (height_ / 2), f_y_};
+	Vector3 direction = M_c_w_ * target;
+	ray.dir_x = direction.x;
+	ray.dir_y =	direction.y;
+	ray.dir_z =	direction.z;
+	ray.time = 0.0f; // time of this ray for motion blur
+	ray.tfar = FLT_MAX; // end of ray segment (set to hit distance)
+
+	ray.mask = 0; // can be used to mask out some geometries for some rays
+	ray.id = 0; // identify a ray inside a callback function
+	ray.flags = 0; // reserved
+
+	return ray;
+}
---
+++ src/pg/pg1_embree/raytracer.cpp
@@ -0,0 +1,194 @@
+#include "stdafx.h"
+#include "raytracer.h"
+#include "objloader.h"
+#include "tutorials.h"
+
+Raytracer::Raytracer( const int width, const int height,
+	const float fov_y, const Vector3 view_from, const Vector3 view_at,
+	const char * config ) : SimpleGuiDX11( width, height )
+{
+	InitDeviceAndScene( config );
+
+	camera_ = Camera( width, height, fov_y, view_from, view_at );
+}
+
+Raytracer::~Raytracer()
+{
+	ReleaseDeviceAndScene();
+}
+
+int Raytracer::InitDeviceAndScene( const char * config )
+{
+	device_ = rtcNewDevice( config );
+	error_handler( nullptr, rtcGetDeviceError( device_ ), "Unable to create a new device.\n" );
+	rtcSetDeviceErrorFunction( device_, error_handler, nullptr );
+
+	ssize_t triangle_supported = rtcGetDeviceProperty( device_, RTC_DEVICE_PROPERTY_TRIANGLE_GEOMETRY_SUPPORTED );
+
+	// create a new scene bound to the specified device
+	scene_ = rtcNewScene( device_ );
+
+	return S_OK;
+}
+
+int Raytracer::ReleaseDeviceAndScene()
+{
+	rtcReleaseScene( scene_ );
+	rtcReleaseDevice( device_ );
+
+	return S_OK;
+}
+
+void Raytracer::LoadScene( const std::string file_name )
+{
+	const int no_surfaces = LoadOBJ( file_name.c_str(), surfaces_, materials_ );
+
+	// surfaces loop
+	for ( auto surface : surfaces_ )
+	{
+		RTCGeometry mesh = rtcNewGeometry( device_, RTC_GEOMETRY_TYPE_TRIANGLE );
+
+		Vertex3f * vertices = ( Vertex3f * )rtcSetNewGeometryBuffer(
+			mesh, RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3,
+			sizeof( Vertex3f ), 3 * surface->no_triangles() );
+
+		Triangle3ui * triangles = ( Triangle3ui * )rtcSetNewGeometryBuffer(
+			mesh, RTC_BUFFER_TYPE_INDEX, 0, RTC_FORMAT_UINT3,
+			sizeof( Triangle3ui ), surface->no_triangles() );
+
+		rtcSetGeometryUserData( mesh, ( void* )( surface->get_material() ) );
+
+		rtcSetGeometryVertexAttributeCount( mesh, 2 );
+
+		Normal3f * normals = ( Normal3f * )rtcSetNewGeometryBuffer(
+			mesh, RTC_BUFFER_TYPE_VERTEX_ATTRIBUTE, 0, RTC_FORMAT_FLOAT3,
+			sizeof( Normal3f ), 3 * surface->no_triangles() );
+
+		Coord2f * tex_coords = ( Coord2f * )rtcSetNewGeometryBuffer(
+			mesh, RTC_BUFFER_TYPE_VERTEX_ATTRIBUTE, 1, RTC_FORMAT_FLOAT2,
+			sizeof( Coord2f ), 3 * surface->no_triangles() );		
+
+		// triangles loop
+		for ( int i = 0, k = 0; i < surface->no_triangles(); ++i )
+		{
+			Triangle & triangle = surface->get_triangle( i );
+
+			// vertices loop
+			for ( int j = 0; j < 3; ++j, ++k )
+			{
+				const Vertex & vertex = triangle.vertex( j );
+
+				vertices[k].x = vertex.position.x;
+				vertices[k].y = vertex.position.y;
+				vertices[k].z = vertex.position.z;
+
+				normals[k].x = vertex.normal.x;
+				normals[k].y = vertex.normal.y;
+				normals[k].z = vertex.normal.z;
+
+				tex_coords[k].u = vertex.texture_coords[0].u;
+				tex_coords[k].v = vertex.texture_coords[0].v;
+			} // end of vertices loop
+
+			triangles[i].v0 = k - 3;
+			triangles[i].v1 = k - 2;
+			triangles[i].v2 = k - 1;
+		} // end of triangles loop
+
+		rtcCommitGeometry( mesh );
+		unsigned int geom_id = rtcAttachGeometry( scene_, mesh );
+		rtcReleaseGeometry( mesh );
+	} // end of surfaces loop
+
+	rtcCommitScene( scene_ );
+}
+
+
+// 640, 480
+Color4f Raytracer::get_pixel( const int x, const int y, const float t )
+{
+	// TODO generate primary ray and perform ray cast on the scene
+	RTCRay ray = camera_.GenerateRay(x, y);
+	
+	RTCHit hit;
+	hit.geomID = RTC_INVALID_GEOMETRY_ID;
+	hit.primID = RTC_INVALID_GEOMETRY_ID;
+	hit.Ng_x = 0.0f; // geometry normal
+	hit.Ng_y = 0.0f;
+	hit.Ng_z = 0.0f;
+
+	// merge ray and hit structures
+	RTCRayHit ray_hit;
+	ray_hit.ray = ray;
+	ray_hit.hit = hit;
+
+	// intersect ray with the scene
+	RTCIntersectContext context;
+	rtcInitIntersectContext(&context);
+	rtcIntersect1(scene_, &context, &ray_hit);
+
+	
+	if (ray_hit.hit.geomID != RTC_INVALID_GEOMETRY_ID)
+	{
+		current_material = surfaces_[ray_hit.hit.geomID]->get_material();
+
+		Color3f diff{0, 0, 0};
+		Color3f spec{0, 0, 0};
+		Color3f normal{0, 0, 0};
+		Color3f oppacity{0, 0, 0};
+		
+		if (current_material->get_texture(current_material->kDiffuseMapSlot) != nullptr)
+			diff = current_material->get_texture(current_material->kDiffuseMapSlot)->get_texel(ray_hit.hit.u, ray_hit.hit.v);
+		else
+			diff = { current_material->diffuse.x, current_material->diffuse.y, current_material->diffuse.z };
+
+
+		return Color4f{diff.r, diff.g, diff.b, 1.0f};
+	}
+
+	float x_ = (float)x;
+	float y_ = (float)y;
+	return Color4f{ x_/ 640.0f, y_ / 480.0f, 0.0f, 1.0f };
+	
+}
+
+int Raytracer::Ui()
+{
+	static float f = 0.0f;
+	static int counter = 0;
+
+	// Use a Begin/End pair to created a named window
+	ImGui::Begin( "Ray Tracer Params" );
+	
+	ImGui::Text( "Surfaces = %d", surfaces_.size() );
+	ImGui::Text( "Materials = %d", materials_.size() );
+	ImGui::Separator();
+	ImGui::Checkbox( "Vsync", &vsync_ );
+	
+	//ImGui::Checkbox( "Demo Window", &show_demo_window ); // Edit bools storing our window open/close state
+	//ImGui::Checkbox( "Another Window", &show_another_window );
+
+	ImGui::SliderFloat( "float", &f, 0.0f, 1.0f ); // Edit 1 float using a slider from 0.0f to 1.0f    
+	//ImGui::ColorEdit3( "clear color", ( float* )&clear_color ); // Edit 3 floats representing a color
+
+	// Buttons return true when clicked (most widgets return true when edited/activated)
+	if ( ImGui::Button( "Button" ) )
+		counter++;
+	ImGui::SameLine();
+	ImGui::Text( "counter = %d", counter );
+
+	ImGui::Text( "Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate );
+	ImGui::End();
+
+	// 3. Show another simple window.
+	/*if ( show_another_window )
+	{
+	ImGui::Begin( "Another Window", &show_another_window ); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+	ImGui::Text( "Hello from another window!" );
+	if ( ImGui::Button( "Close Me" ) )
+	show_another_window = false;
+	ImGui::End();
+	}*/
+
+	return 0;
+}
